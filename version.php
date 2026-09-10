02a544986e6740211551fd6393ef0c024e4f9bd7
KevinPulido <kev_pulbravo@hotmail.com>
2026-09-10 12:49:15 -0500

    Feat(local) : Se aumenta la versión del plugin

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edukav';
-$plugin->version   = 2026021010;
+$plugin->version   = 2026090701;
 $plugin->requires  = 2024100708; 
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1';
